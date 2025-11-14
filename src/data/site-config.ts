62a512c94d666597d19936251cd8a09902add1b8
Sandhu40 <150025721+Sandhu40@users.noreply.github.com>
2025-11-14 16:52:08 +0000

    Change site title

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -6,11 +6,11 @@ const siteConfig: SiteConfig = {
     website: 'https://example.com',
     avatar: {
         src: avatar,
-        alt: 'Ethan Donovan'
+        alt: 'Maya Paul'
     },
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    title: 'Maya Paul',
+    subtitle: 'Social Worker',
+    description: 'A Social Work reflection blog',
     image: {
         src: '/dante-preview.jpg',
         alt: 'Dante - Astro.js and Tailwind CSS theme'
@@ -66,8 +66,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
+        title: 'Hi, I'm Maya – a Social Worker!',
+        text: "I write about my studies, ASYE, and life in practice.",
         image: {
             src: hero,
             alt: 'A person sitting at a desk in front of a computer'
